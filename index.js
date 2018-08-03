32f151908f5b56b11b46cbb2376da9c22f469c71
Nicholas Wagner <nicholas@secretsignal.com>
2018-08-03 16:07:25 -0500

    just say no to copy-pasta-ing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 5000;
 var app = express();
 
 function setAllowWebUSBPolicy(res, path) {
-  res.setHeaders('Feature-Policy', 'usb "*";');
+  res.setHeader('Feature-Policy', 'usb "*";');
 }
 
 app.use(
