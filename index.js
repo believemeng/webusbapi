cedd0c57f1a1b0da3cd1efc6e960daed06adde62
Nicholas Wagner <nicholas@secretsignal.com>
2018-08-03 16:20:33 -0500

    *

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 5000;
 var app = express();
 
 function setAllowWebUSBPolicy(res, path) {
-  res.setHeader('Feature-Policy', 'usb "self";');
+  res.setHeader('Feature-Policy', 'usb *;');
 }
 
 app.use(
